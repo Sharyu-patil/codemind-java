a3e020a607e21e5bf8abf482fc2ad6e815e3a62f
Sharyu-patil <100119880+Sharyu-patil@users.noreply.github.com>
2022-10-13 14:16:12 +0530

    import java.util.Scanner; class Pattern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++)         {             for(int j=0;j<n;j++)             {                 if(i==j)                 {                     System.out.print("0");                 }                 else                 {                     System.out.print("x");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -9,13 +9,13 @@ class Pattern
         {
             for(int j=0;j<n;j++)
             {
-                if(j==0||i==j||j==n-1)
+                if(i==j)
                 {
-                    System.out.print("* ");
+                    System.out.print("0");
                 }
                 else
                 {
-                    System.out.print("  ");
+                    System.out.print("x");
                 }
             }
             System.out.println();
