2906856b2f9af9b684a92fb730a4bcbc33c4e9a4
Sharyu-patil <100119880+Sharyu-patil@users.noreply.github.com>
2022-11-03 01:04:28 -0800

    import java.util.Scanner; class pat {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int i,j;         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                 System.out.print(i+" ");             }             System.out.println();         }     } }

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class pat
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
